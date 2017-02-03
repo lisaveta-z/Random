bb3d99bd2d92d7e2d3a36994217b8b94f0e76195
lisaveta-z <lisaveta.z@rambler.ru>
2017-02-03 22:53:28 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,14 @@
+﻿var letter = document.getElementById("letter");
+var number = document.getElementById("number");
+var generator = document.getElementById("rand");
+
+var lettersArray = ["a", "б", "в", "г", "д", "е", "ж", "з"];
+
+function randomInteger(min, max) {
+    var rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+}
+
+number.addEventListener("click", function () { generator.textContent = randomInteger(1, 9) });
+letter.addEventListener("click", function () { generator.textContent = lettersArray[randomInteger(0, 7)]; });
